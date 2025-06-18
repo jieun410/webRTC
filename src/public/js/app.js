960ed149124b6056251ca454f4d73419cea3d9f2
Jieun Lee <easyeun410@gmail.com>
2025-06-18 16:29:30 +0900

    Room Count part Two

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -64,3 +64,16 @@ socket.on("bye", (left) => {
 });
 
 socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+  const roomList = welcome.querySelector("ul");
+  roomList.innerHTML = ""; // 이전 목록 초기화
+  if (rooms.length === 0) {
+    return;
+  }
+  rooms.forEach((room) => {
+    const li = document.createElement("li");
+    li.innerText = room;
+    roomList.append(li);
+  });
+});
